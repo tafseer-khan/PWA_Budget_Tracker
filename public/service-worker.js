969914513730fe2042c73fe7cf5bca130d196b88
tafseer-khan <tafseer.k.khan@gmail.com>
2021-03-04 16:32:23 -0600

    Updated service worker added db.js file for posting

---
+++ public/service-worker.js
@@ -0,0 +1,74 @@
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/index.js",
+    "/manifest.json",
+    "/style.css",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
+    "/db.js"
+];
+
+const STATIC_CACHE = "static-cache-v1";
+const RUNTIME_CACHE = "runtime-cache";
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches
+            .open(STATIC_CACHE)
+            .then(cache => cache.addAll(FILES_TO_CACHE))
+            .then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener("activate", event => {
+    const currentCaches = [STATIC_CACHE, RUNTIME_CACHE];
+    event.waitUntil(
+        caches
+            .keys()
+            .then(cacheNames => {
+                // return array of cache names that are old to delete
+                return cacheNames.filter(
+                    cacheName => !currentCaches.includes(cacheName)
+                );
+            })
+            .then(cachesToDelete => {
+                return Promise.all(
+                    cachesToDelete.map(cacheToDelete => {
+                        return caches.delete(cacheToDelete);
+                    })
+                );
+            })
+            .then(() => self.clients.claim())
+    );
+});
+
+self.addEventListener("fetch", evt => {
+    if(evt.request.url.includes('/api/')) {
+        console.log('[Service Worker] Fetch(data)', evt.request.url);
+    
+evt.respondWith(
+                caches.open(DATA_CACHE_NAME).then(cache => {
+                return fetch(evt.request)
+                .then(response => {
+                    if (response.status === 200){
+                        cache.put(evt.request.url, response.clone());
+                    }
+                    return response;
+                })
+                .catch(err => {
+                    return cache.match(evt.request);
+                });
+            })
+            );
+            return;
+        }
+
+evt.respondWith(
+    caches.open(CACHE_NAME).then( cache => {
+      return cache.match(evt.request).then(response => {
+        return response || fetch(evt.request);
+      });
+    })
+  );
+});
