683659d73cf3956d8b3d1e549f612e14b9bf5f74
tafseer-khan <tafseer.k.khan@gmail.com>
2021-03-04 17:03:21 -0600

    server update

--- server.js
+++ server.js
@@ -15,11 +15,13 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost/budget`, {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/budget',
+  {useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false}
+)
 
 // routes
 app.use(require("./routes/api.js"));
