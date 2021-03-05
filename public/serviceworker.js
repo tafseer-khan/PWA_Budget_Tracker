e09efc740283d5dbc6a957c6d5529174de9eba94
tafseer-khan <tafseer.k.khan@gmail.com>
2021-03-05 02:14:31 -0600

    more typos

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -10,7 +10,7 @@ const FILES_TO_CACHE = [
 ];
 
 const CACHE_NAME = "static-cache-v2";
-const DATA_CACHE_NAME = "data-cache-v1";
+const RUNTIME_CACHE = "runtime-cache";
 
 // install
 self.addEventListener("install", event => {
